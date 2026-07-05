95af14ea3c8d8dc9b9684dab982cf7fc80e29183
sriraam13 <sriraamsp@gmail.com>
2026-07-05 11:34:30 +0530

    updated backend1

--- add_item_code.py
+++ add_item_code.py
@@ -1,10 +1,7 @@
 import os
 from sqlalchemy import create_engine, text
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://food_admin:foodadmin%40123@banking-db.cnkegcm24ikf.ap-south-2.rds.amazonaws.com:5432/food_ordering_db",
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
